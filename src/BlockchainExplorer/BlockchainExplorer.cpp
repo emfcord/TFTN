3be4db6223ad751785a6b1c494f7187a808fdc0f
TFTNetwork <44827064+TFTNetwork@users.noreply.github.com>
2019-03-24 12:22:51 -0300

    Zawy LWMA difficulty algorithm added

--- src/BlockchainExplorer/BlockchainExplorer.cpp
+++ src/BlockchainExplorer/BlockchainExplorer.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers, The TFTN developers
+// Copyright (c) 2012-2019, The CryptoNote developers, The Bytecoin developers, The TFTN developers.
 //
 // This file is part of Bytecoin.
 //
